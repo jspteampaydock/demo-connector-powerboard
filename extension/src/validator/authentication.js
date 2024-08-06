e3ab2a80163e9f7ce48a16003338baca5283daa3
paydock.jsp.development <paydock.jsp.development@jetsoftpro.com>
2024-08-06 11:09:05 +0300

    Fixed problem with AuthorizationHeader

--- extension/src/validator/authentication.js
+++ extension/src/validator/authentication.js
@@ -1,7 +1,6 @@
 import config from '../config/config.js'
 
 function hasValidAuthorizationHeader(authTokenString) {
-    if (!authTokenString || authTokenString.indexOf(' ') < 0) return false
     return (config.getAuthorizationHeaderValue() === authTokenString)
 }
 
