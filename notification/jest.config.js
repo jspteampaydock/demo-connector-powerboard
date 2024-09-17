a67646c9f52e39cc23b75541ca2a9b73a70cac25
jspteampaydock <157017480+jspteampaydock@users.noreply.github.com>
2024-09-17 09:07:50 +0300

    Fixed problem with notification

--- notification/jest.config.js
+++ notification/jest.config.js
@@ -7,6 +7,18 @@ export default {
         [
             '//node_modules'
         ],
+    modulePathIgnorePatterns: [
+        "<rootDir>/src/config/",
+        "<rootDir>/src/validator/error-messages.js",
+    ],
+    coveragePathIgnorePatterns: [
+        "<rootDir>/src/config/",
+        "<rootDir>/src/validator/error-messages.js",
+    ],
+    testPathIgnorePatterns: [
+        "<rootDir>/src/config/",
+        "<rootDir>/src/validator/error-messages.js",
+    ],
     displayName: 'Tests Javascript Application - Powerboard Extension',
     moduleDirectories: ['node_modules', 'src'],
     testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
